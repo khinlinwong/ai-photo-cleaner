40b727c1a18fcc73aae2526d97219c320cb2c376
khinlinwong <khinlinwong@gmail.com>
2026-06-15 16:28:13 +1200

    chore: align quick start package id for next external build

--- src/lib/config/appVersion.ts
+++ src/lib/config/appVersion.ts
@@ -7,4 +7,4 @@ export const APP_IDENTITY_PATCH = "d107a13";
 export const APP_VERSION_LABEL = "Alpha Candidate 7b36ab6 + identity patch";
 export const APP_VERSION_SHORT_LABEL = "Alpha Candidate 7b36ab6+";
 export const APP_BUILD_NOTES = "200 batch, Results filters, A/B workflow cues, build identity";
-export const APP_CURRENT_PACKAGE_ID = "QuickStart-e8b2a81";
+export const APP_CURRENT_PACKAGE_ID = "QuickStart-PackageScopedSeenKey-1";
